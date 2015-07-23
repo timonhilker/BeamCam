062c35e478432f345643402c40bc55b6f8b61c81
Michael <michael.hoese@mpq.mpg.de>
2015-07-23 18:44:59 +0200

    WIP

--- FirstTest.py
+++ FirstTest.py
@@ -7,6 +7,8 @@ Created on Thu Jul 16 10:18:13 2015
 
 from ctypes import *
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 
 class CameraKey(Structure):
@@ -158,7 +160,7 @@ class VRmagicUSBCam_API:
 
 				format = ImageFormat()
 				format.m_width = 754
-				format.m_height = 480
+				format.m_height = 482
 				format.m_color_format = 4
 				format.m_image_modifier = 0
 
@@ -175,12 +177,21 @@ class VRmagicUSBCam_API:
 				color = ''.join(color)
 				print 'Color format: ', color
 
-				# image_p = POINTER(Image)
+				pixeldepth = c_uint(0)
+
+
+				Error = self.dll.VRmUsbCamGetPixelDepthFromColorFormat(format.m_color_format,byref(pixeldepth))
+
+				print 'Pixel Depth: ', pixeldepth.value
+
 				self.dll.VRmUsbCamNewImage.argtypes = [POINTER(POINTER(Image)),ImageFormat]
 		
 				self.image_p = POINTER(Image)()
 
 				Error = self.dll.VRmUsbCamNewImage(byref(self.image_p),format)
+
+
+				print 'Pitch: ', self.image_p.contents.m_pitch
 
 				if Error==0:
 					self.ShowErrorInformation()
@@ -190,6 +201,137 @@ class VRmagicUSBCam_API:
 
 				if Error==1:
 					print'Image taken!'
+
+					ImageList = list(self.image_p.contents.mp_buffer[0:((format.m_width-1)*int(pixeldepth.value)+(format.m_height-1)*int(self.image_p.contents.m_pitch))])
+					# print ImageList[0:10]
+					# print len(ImageList)
+					ImageList = [ord(i) for i in ImageList]
+					print len(ImageList)
+
+					self.ImageArray = np.array(ImageList)
+					self.ImageArray = np.reshape(self.ImageArray,(format.m_height,int(self.image_p.contents.m_pitch)))
+					self.ImageArray = self.ImageArray[:,:format.m_width]
+
+
+					# for j in range(format.m_height):
+					# 	for i in range(format.m_width):
+					# 		self.ImageArray[j,i] = ord(self.image_p.contents.mp_buffer[j*int(pixeldepth.value)+i*int(self.image_p.contents.m_pitch)])
+
+							# print ord(ImageList[i*int(pixeldepth.value)+j*int(self.image_p.contents.m_pitch)])
+
+					plt.figure()
+					plt.imshow(self.ImageArray, cmap = cm.Greys_r)
+
+
+
+
+					name_p = c_char_p('Test.png')
+					Error = self.dll.VRmUsbCamSavePNG(name_p,self.image_p,c_int(0))
+					if Error==0:
+						self.ShowErrorInformation()
+					if Error==1:
+						print'Image saved!'
+
+				Error = self.dll.VRmUsbCamFreeImage(byref(self.image_p))
+
+				if Error==0:
+					self.ShowErrorInformation()
+
+
+
+				
+
+
+
+
+		Error = self.dll.VRmUsbCamFreeDeviceKey(byref(self.key))
+		if Error==0:
+			self.ShowErrorInformation()
+
+		Error = self.dll.VRmUsbCamCloseDevice(self.CamIndex)
+		if Error==0:
+			self.ShowErrorInformation()
+
+
+	def TakePictureGrabbing(self,keytest=0):
+		if keytest==0:
+			print 'No valid key available!'
+		elif self.key.contents.m_busy!=0:
+			print 'Camera is busy!'
+		else:
+			Error = self.dll.VRmUsbCamOpenDevice(self.key,byref(self.CamIndex))
+			if Error==0:
+				self.ShowErrorInformation()
+			else:
+				print 'Device opend successfully'
+
+				format = ImageFormat()
+				format.m_width = 754
+				format.m_height = 482
+				format.m_color_format = 4
+				format.m_image_modifier = 0
+
+				inf = POINTER(c_char)()
+
+				Error = self.dll.VRmUsbCamGetStringFromColorFormat(format.m_color_format,byref(inf))
+
+				color = []
+				i = 0
+				
+				while inf[i] != '\0':
+					color.append(inf[i])
+					i += 1
+				color = ''.join(color)
+				print 'Color format: ', color
+
+				pixeldepth = c_uint(0)
+
+
+				Error = self.dll.VRmUsbCamGetPixelDepthFromColorFormat(format.m_color_format,byref(pixeldepth))
+
+				print 'Pixel Depth: ', pixeldepth.value
+
+				self.dll.VRmUsbCamLockNextImageEx.argtypes = [c_uint,c_uint,POINTER(POINTER(Image)),POINTER(c_uint)]
+		
+				self.image_p = POINTER(Image)()
+
+				framesdropped = c_uint(0)
+
+				Error = self.dll.VRmUsbCamStart(c_uint(0))
+
+				Error = self.dll.VRmUsbCamLockNextImageEx(c_uint(0),c_uint(0),byref(self.image_p),byref(framesdropped))
+
+				Error = self.dll.VRmUsbCamStop(c_uint(0))
+
+
+				print 'Pitch: ', self.image_p.contents.m_pitch
+
+				if Error==0:
+					self.ShowErrorInformation()
+
+				
+
+
+				if Error==1:
+					print'Image taken!'
+
+					# ImageList = self.image_p.contents.mp_buffer[0:(format.m_width*int(pixeldepth.value)+format.m_height*int(self.image_p.contents.m_pitch))]
+					# print ImageList[0:10]
+					# print len(ImageList)
+
+					self.ImageArray = np.zeros((format.m_height,format.m_width))
+					for j in range(5):#format.m_height):
+						for i in range(5):#format.m_width):
+							self.ImageArray[j,i] = ord(self.image_p.contents.mp_buffer[j*int(pixeldepth.value)+i*int(self.image_p.contents.m_pitch)])
+
+							# print ord(ImageList[i*int(pixeldepth.value)+j*int(self.image_p.contents.m_pitch)])
+
+					plt.figure()
+					plt.imshow(self.ImageArray, cmap = cm.Greys_r)
+
+
+
+
 					name_p = c_char_p('Test.png')
 					Error = self.dll.VRmUsbCamSavePNG(name_p,self.image_p,c_int(0))
 					if Error==0:
@@ -232,3 +374,4 @@ if __name__=="__main__":
 	keycheck = check.GetDeviceKeyListEntry()
 	check.GetDeviceInformation(keycheck)
 	check.TakePicture(keycheck)
+	plt.show()
